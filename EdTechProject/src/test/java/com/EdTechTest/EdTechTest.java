d6249c9dcaa861c0836af3afcf0a70312b50c52d
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-24 01:20:20 -0700

    uml diagrams

---
+++ EdTechProject/src/test/java/com/EdTechTest/EdTechTest.java
@@ -0,0 +1,11 @@
+package com.EdTechTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdTechTest {
+   @Test
+    void contextLoads() {
+    }
+}
