aefc5b502003766b12d64e347932b2737ed7516c
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-24 01:07:42 -0700

    setting up environment

---
+++ EdTechProject/src/main/java/com/EdTech/EdTechProjectMain.java
@@ -0,0 +1,11 @@
+package com.EdTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdTechProjectMain {
+    static void main(String[] args) {
+        SpringApplication.run(EdTechProjectMain.class, args);
+    }
+}
